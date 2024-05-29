849770d34543e5a7ad23fb9c2eed3aa923ec68b9
Manan21st <manan9346@gmail.com>
2024-05-30 00:15:09 +0530

    landing page complete and progress on about page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,9 +1,25 @@
 import './About.css';
+import manan_full from '../../assets/manan_full.jpg';
 function About(){
     return (
         <div className="about">
-            <h1>About</h1>
-            <p>This is the About page</p>
+            <div className='line'></div>
+            <div className='about-body'>    
+                <div className='about-description'>
+                    <h3>About Me</h3>
+                    <p>
+                        My name is Manan Agrawal. I am a CS Undergrad based in Bengaluru, India.
+                        <br />
+                        <br />
+                        I am passionate about Backend Development, Data Science and Competitive Programming. 
+                        I have built projects in Python, Java and JavaScript. I spend my leisure time playing video games and watching anime. Right now I am learning MERN stack and CS fundamentals.
+                        Everyday I strive to learn something new and improve my skills.
+                    </p>
+                </div>
+                <div className='about-img'>
+                    <img src={manan_full} />
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,5 +1,9 @@
 import './Landing.css';
 import manan_potrait from '../../assets/manan_potrait.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 function Landing(){
     return (
@@ -7,17 +11,26 @@ function Landing(){
             <div className='landing-cont'>
                 <div className='left-cont'>
                     <div className='container-contents'>
-                        <a href='google.com'></a>
-                        <a href='google.com'></a>
-                        <a href='google.com'></a>
+                        <a href='https://www.linkedin.com/in/manan-agrawal-bb6b19291/'>
+                            <FontAwesomeIcon icon={faLinkedin} className='social-media'/>
+                        </a>
+                        <a href='https://github.com/Manan21st'>
+                            <FontAwesomeIcon icon={faGithub} className='social-media'/>
+                        </a>
+                        <a href='https://www.youtube.com/@NaughtyJoke'>
+                            <FontAwesomeIcon icon={faYoutube} className='social-media'/>
+                        </a>
                     </div>
                 </div>
-                <img src={manan_potrait} alt className='landing-img'/>
+                <img src={manan_potrait} alt='Manan' className='landing-img'/>
                 <div className="right-cont">
                     <div className='container-contents'>
-                        <h5>Computer Science Undergrad</h5>
+                        <h6>Computer Science Undergrad</h6>
                         <h1>Manan Agrawal</h1>
-                        <div className='btn'>Download Resume</div>
+                        <p>"Trust is a garden; nurture it with honesty, and it will bloom with loyalty. Neglect it, and only weeds of doubt will grow."</p>
+                        <a href='www.google.com' download={manan_potrait}>
+                            <button className='button-icon'>Download Resume</button>
+                        </a>
                     </div>
                 </div>
             </div>
